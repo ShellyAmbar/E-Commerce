5d44ce3ab198ef4bc930b5928cf6bf2bc7f72a5a
Shelly Ambar <42389001+ShellyAmbar@users.noreply.github.com>
2024-02-06 09:19:57 +0200

    iniitalizing

---
+++ my-app/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <Button variant="outline">Button</Button>
+
+      <p>Hello Admin</p>
+    </>
+  );
+}
