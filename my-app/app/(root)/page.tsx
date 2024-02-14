69950b2a5674db6dfd2e6ec5b15e6335dfc1d749
Shelly Ambar <42389001+ShellyAmbar@users.noreply.github.com>
2024-02-14 13:20:34 +0200

    iniitalizing

--- my-app/app/(root)/page.tsx
+++ my-app/app/(root)/page.tsx
@@ -2,10 +2,10 @@ import {Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <>
+    <div>
       <Button variant="outline">Button</Button>
 
       <p>Hello Admin</p>
-    </>
+    </div>
   );
 }
